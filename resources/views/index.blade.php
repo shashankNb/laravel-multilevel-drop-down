53ed3dbe2d50d69b969f7193ab27dfc6a110f387
Shashank Bhattarai <ex.shashank.bhattarai@gmail.com>
2020-11-21 09:48:00 +0545

    Added if else condition to hide menus if there are no records in the database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,9 +21,11 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand">Laravel 7</a>
+        @if(count($menulist) > 0)
         <ul class="navbar-nav mr-auto">
             @each('submenu', $menulist, 'menu', 'empty')
         </ul>
+        @endif
     </nav>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
